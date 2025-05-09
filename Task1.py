3eecb336bba13e4731762aab72611f6477f5410e
sersim33 <158172968+sersim33@users.noreply.github.com>
2025-05-09 11:59:54 +0300

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,1 +1,88 @@
+import string
+from concurrent.futures import ThreadPoolExecutor
+from collections import defaultdict
 
+import requests
+import matplotlib.pyplot as plt
+
+
+def get_text(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Перевірка на помилки HTTP
+        return response.text
+    except requests.RequestException as e:
+        return None
+
+
+# Функція для видалення знаків пунктуації
+def remove_punctuation(text):
+    return text.translate(str.maketrans("", "", string.punctuation))
+
+
+def map_function(word):
+    return word.lower(), 1  # Зведення до нижнього регістру
+
+
+def shuffle_function(mapped_values):
+    shuffled = defaultdict(list)
+    for key, value in mapped_values:
+        shuffled[key].append(value)
+    return shuffled.items()
+
+
+def reduce_function(key_values):
+    key, values = key_values
+    return key, sum(values)
+
+
+# Функція для візуалізації топ N слів
+def visualize_top_words(word_counts, top_n=10):
+    sorted_words = sorted(word_counts.items(), key=lambda item: item[1], reverse=True)[:top_n]
+    words, counts = zip(*sorted_words)
+
+    plt.figure(figsize=(10, 6))
+    plt.bar(words, counts, color='green')
+    plt.title(f"Top {top_n} most frequent words")
+    plt.xlabel("Words")
+    plt.ylabel("Counts")
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
+
+
+# Виконання MapReduce
+def map_reduce(text, search_words=None):
+    # Видалення знаків пунктуації
+    text = remove_punctuation(text)
+    words = text.split()
+
+    # Якщо задано список слів для пошуку, враховувати тільки ці слова
+    if search_words:
+        search_words = set(word.lower() for word in search_words)
+        words = [word for word in words if word.lower() in search_words]
+
+    # Паралельний Маппінг
+    with ThreadPoolExecutor() as executor:
+        mapped_values = list(executor.map(map_function, words))
+
+    # Крок 2: Shuffle
+    shuffled_values = shuffle_function(mapped_values)
+
+    # Паралельна Редукція
+    with ThreadPoolExecutor() as executor:
+        reduced_values = list(executor.map(reduce_function, shuffled_values))
+
+    return dict(reduced_values)
+
+
+if __name__ == "__main__":
+    # Вхідний текст для обробки
+    url = "https://gutenberg.net.au/ebooks01/0100021.txt"
+    text = get_text(url)
+    if text:
+        # Виконання MapReduce на вхідному тексті
+        word_counts = map_reduce(text)
+
+        print("Топ 10 слів у тексті:")
+        visualize_top_words(word_counts, top_n=10)
